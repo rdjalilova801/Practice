a0420eb1350147c1a8ea56c984bd6402cb7a0cce
rdjalilova801 <rdjalilova801@gmail.com>
2022-12-15 19:57:06 +0500

    R_House

---
+++ js/main.js
@@ -0,0 +1,103 @@
+let images = document.querySelector(".section")
+let main = document.querySelector(".main")
+add()
+function add() {
+    images.innerHTML =""
+    massiv_img.map((item,index) =>{
+
+        let col = document.createElement("div")
+        col.classList.add("row")
+
+        let card = document.createElement("div")
+        card.classList.add("text-center", "text-warning")
+
+        // let img2 = document.createElement("img")
+        // img2.classList.add("col-6")
+        // img2.src = "../img/3-16 1header_img.png"
+        // img2.alt = "Picture"
+
+        let img = document.createElement("img")
+        img.src = item.img
+        img.alt = "Picture"
+        img.setAttribute("onclick", `almash(${index})`)
+        img.classList.add("images")
+
+        let h_3 = document.createElement("h3")
+        h_3.textContent = item.name
+
+        let h_5 = document.createElement("h5")
+        h_5.textContent = item.narx
+
+        
+        card.appendChild(img)
+        card.appendChild(h_3)
+        card.appendChild(h_5)
+        col.appendChild(card)
+        images.appendChild(col)
+    })
+}
+function almash(son) {
+    massiv_img[0].src = massiv_img[son].src
+    console.log(son);
+    add()  
+}
+run()
+function run() {
+    main.innerHTML=""
+    massiv_img.map((item,index) =>{
+        let card = document.createElement("div")
+        card.classList.add("card", "text-center", "text-warning", "p-5","card1" , "bg-dark","mb-5","mt-4")
+
+        let row = document.createElement("div")
+        row.classList.add("row")
+
+        let col1 = document.createElement("div")
+        col1.classList.add("col-4")
+
+        let col2 = document.createElement("div")
+        col2.classList.add("col-6")
+        
+        let button = document.createElement("button")
+        button.classList.add("btn", "btn-warning", "fw-bold", "text-light", "form-control","mt-3")
+        button.textContent = "Uy sotib olish"
+
+        let h4name = document.createElement("h4")
+        h4name.textContent = item.name
+        h4name.classList.add("py-3")
+
+        let btncha = document.createElement("button")
+        btncha.classList.add("btn","btncha", "btn-outline-warning", "fw-bold", "text-light")
+        btncha.textContent = "Ko`proq"
+
+        let h5 = document.createElement("h5")
+        h5.textContent = "Uyning narxi"
+        h5.classList.add("text-light","py-3")
+
+        let div = document.createElement("div")
+
+        let h05 = document.createElement("h5")
+        h05.textContent = "Qurilish rejasi"
+        h05.classList.add("text-light")
+
+        let hr = document.createElement("hr")
+        hr.classList.add("text-warning", "bg-warning")
+
+        let h5narx = document.createElement("h5")
+        h5narx.textContent = item.narx
+        h5narx.classList.add("fs-4")
+
+        col1.appendChild(h4name)
+        col1.appendChild(btncha)
+        col1.appendChild(h5)
+        col2.appendChild(div)
+        col2.appendChild(h05)
+        col2.appendChild(hr)
+        col2.appendChild(h5narx)
+        row.appendChild(col1)
+        row.appendChild(col2)
+        card.appendChild(row)
+        card.appendChild(button)
+        main.appendChild(card)
+        
+    })
+}
---
+++ js/massiv.js
@@ -0,0 +1,22 @@
+let massiv_img = [
+    // {
+    //     img:"img/3-16 1header_img.png",
+    //     name:"Aegina",
+    //     narx:222000
+    // },
+    {
+        img:"img/11 1.png",
+        name:"Afrodita",
+        narx:422000
+    },
+    {
+        img:"img/23 1.png",
+        name:"Likiya",
+        narx:332000
+    },
+    {
+        img:"img/30 1.png",
+        name:"Salamina",
+        narx:432000
+    }
+]
